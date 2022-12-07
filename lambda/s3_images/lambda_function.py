b0ef33960e919c7718e4fba41cdcd1f0fbd19c86
Wataru823 <voyage0823@gmail.com>
2022-12-08 00:42:58 +0900

    refactor: 不要なコードを削除, Client,Eyeクラスを作成し繰り返しを削除, docstringsを追加

--- lambda/s3_images/lambda_function.py
+++ lambda/s3_images/lambda_function.py
@@ -1,55 +1,36 @@
 import io
+import os
 
 import urllib.parse
 import boto3
 from PIL import Image
 
-print('Loading function')
-
+OUTPUT_BUCKET = 'output用のバケット名'
+emoji_type = 'blush' # 絵文字の種類を選ぶ grinning, heart_eyes, laughing, pleading_face
 
 def lambda_handler(event, context):
-    input_bucket = event['Records'][0]['s3']['bucket']['name'] # inputの画像があるバケット名
-    output_bucket = 'output-images-ikeda' # output用のバケット名
-    image_dir = 'images/' # input画像のディレクトリ
-    tmp_key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8') # input画像のファイル名
-    # tmp_key = 'images/face.jpg'
-    key = tmp_key.replace(image_dir, '')
-    print(tmp_key, key)
-    image_path = image_dir + key  # s3に入れた顔画像のpath
-    emoji_type = 'blush' # 絵文字の種類を選ぶ grinning, heart_eyes, laughing, pleading_face
     emoji_file_path = f'images/emoji/{emoji_type}.png' # 絵文字スタンプで使う{emoji_type}画像
     glass_file_path = 'images/glass/glass.png'
 
-    session = boto3.session.Session()
-    response = session.client('s3').list_objects_v2(Bucket=input_bucket)
-    rekognition = session.client('rekognition')
-    s3_resource = session.resource('s3')
+    client = Client(event)
+    image = client.get_s3_image(client.image_path)
+    emoji_image = client.get_s3_image(emoji_file_path)
+    glass_image = client.get_s3_image(glass_file_path)
+    labels = client.get_rekognition_labels()
 
-
-    for r in response['Contents']:
-        if r['Key'] == emoji_file_path:
-            emoji_image = get_s3_image(s3_resource, input_bucket, emoji_file_path)
-        if r['Key'] == emoji_file_path:
-            glass_image = get_s3_image(s3_resource, input_bucket, glass_file_path)
-            print("get emoji")
-
-    for r in response['Contents']:
-        if r['Key'] == image_path:
-            print("find " + r['Key'])
-
-            image = get_s3_image(s3_resource, input_bucket, image_path)
+    for r in client.response['Contents']:
+        if r['Key'] == client.image_path:
             output_emoji = image.copy()
             output_glass = image.copy()
             output_mozaiku = image.copy()
-            labels = get_rekognition_labels(rekognition, input_bucket, image_path)
 
             for label in labels['FaceDetails']:
                 draw_emoji(label, output_emoji, emoji_image)
-                draw_glass(label, output_glass, glass_image)
-                print(f"{emoji_type} done")
-                draw_mozaiku(label, output_mozaiku)
+                eye = Eye(label)
+                eye.draw_glass(output_glass, glass_image)
+                eye.draw_mozaiku(output_mozaiku)
 
-            tmpkey = image_path.replace('/', '')
+            tmpkey = client.image_path.replace('/', '')
             input_upload_path = '/tmp/input-{}'.format(tmpkey)
             emoji_upload_path = '/tmp/emoji-{}'.format(tmpkey)
             glass_upload_path = '/tmp/glass-{}'.format(tmpkey)
@@ -58,128 +39,184 @@ def lambda_handler(event, context):
             output_emoji.save(emoji_upload_path)
             output_glass.save(glass_upload_path)
             output_mozaiku.save(mozaiku_upload_path)
+
+            image_file = os.path.basename(client.image_path) # 画像のファイル名
             s3_client = boto3.client('s3')
-            s3_client.upload_file(input_upload_path, output_bucket, 'input/'+key)
-            s3_client.upload_file(emoji_upload_path, output_bucket, f'output/emoji/{emoji_type}_'+key)
-            s3_client.upload_file(glass_upload_path, output_bucket, f'output/glass/glass_'+key)
-            s3_client.upload_file(mozaiku_upload_path, output_bucket, 'output/mozaiku/mozaiku_'+key)
-            print('upload done')
+            s3_client.upload_file(input_upload_path, OUTPUT_BUCKET, 'input/'+image_file)
+            s3_client.upload_file(emoji_upload_path, OUTPUT_BUCKET, f'output/emoji/{emoji_type}_'+image_file)
+            s3_client.upload_file(glass_upload_path, OUTPUT_BUCKET, f'output/glass/glass_'+image_file)
+            s3_client.upload_file(mozaiku_upload_path, OUTPUT_BUCKET, 'output/mozaiku/mozaiku_'+image_file)
             break
 
     return "Thanks"
 
 
-def get_rekognition_labels(rekognition, input_bucket: str, image_path: str):
-    labels = rekognition.detect_faces(
-            Image={
-                "S3Object":
-                    {"Bucket": input_bucket,
-                    "Name": image_path}},
-            Attributes=['ALL']
-            )
-    return labels
+def draw_emoji(label: dict, output_emoji: Image.Image, emoji_image: Image.Image):
+    """画像に絵文字を描画する関数
 
-
-def get_s3_image(s3_resource, input_bucket: str, image_path: str):
-    # S3から画像の読み込み
-    s3_object = s3_resource.Object(input_bucket, image_path).get()
-    stream = io.BytesIO(s3_object['Body'].read())
-    output_emoji = Image.open(stream)
-    return output_emoji
-
-
-def draw_emoji(label, output_emoji, emoji_image):
+    Args:
+        label (dict): Rekognitionデータ
+        output_emoji (Image.Image): 出力する画像
+        emoji_image (Image.Image): 描画する際に用いる絵文字の画像
+    """
     imgWidth, imgHeight = output_emoji.size
     box = label['BoundingBox']
-    left = imgWidth * box['Left']
-    top = imgHeight * box['Top']
-    width = imgWidth * box['Width']
-    height = imgHeight * box['Height']
+    left = int(imgWidth * box['Left'])
+    top = int(imgHeight * box['Top'])
+    width = int(imgWidth * box['Width'])
+    height = int(imgHeight * box['Height'])
 
-    emoji_size = max(int(width), int(height))
-    face_min = min(int(width), int(height))
+    emoji_size = max(width, height)
+    face_min = min(width, height)
 
-    left -= (emoji_size-face_min)/2
-    pos = (int(left), int(top))
+    left -= int((emoji_size-face_min)/2)
     emoji_image = emoji_image.resize((emoji_size, emoji_size))
-    output_emoji.paste(emoji_image, pos, emoji_image)
+    output_emoji.paste(emoji_image, (left, top), emoji_image)
+
+class Eye:
+    def __init__(self, label: dict):
+        """Rekognitionデータから目の情報を取得
+
+        Args:
+            label (dict): Rekognitionデータ
+
+        """
+
+        self.boxes = label['Landmarks'] # 目なら'Landmarks'を指定
+
+        for box in self.boxes:
+            if box['Type'] == 'leftEyeUp':
+                self.eye_left_top = box['Y']
+            if box['Type'] == 'leftEyeDown':
+                self.eye_left_bottom = box['Y']
+            if box['Type'] == 'rightEyeUp':
+                self.eye_right_top = box['Y']
+            if box['Type'] == 'rightEyeDown':
+                self.eye_right_bottom = box['Y']
+            if box['Type'] == 'leftEyeLeft':
+                self.eye_left_left = box['X']
+            if box['Type'] == 'rightEyeRight':
+                self.eye_right_right = box['X']
+
+    def draw_mozaiku(self, output_mozaiku: Image.Image):
+        """画像にモザイクを描画する関数
+
+        Args:
+            output_mozaiku (Image.Image): 出力する画像
+
+        """
+
+        imgWidth, imgHeight = output_mozaiku.size
+
+        eye_top = int(min(self.eye_left_top, self.eye_right_top) * imgHeight)
+        eye_bottom = int(max(self.eye_left_bottom, self.eye_right_bottom) * imgWidth)
+        eye_left_left = int(self.eye_left_left * imgWidth)
+        eye_right_right = int(self.eye_right_right * imgWidth)
+
+        # モザイク範囲を少し広げる
+        margin = (eye_bottom - eye_top) * 1
+        eye_top -= margin
+        eye_bottom += int(margin * 1.5)
+        eye_left_left -= margin * 2
+        eye_right_right += margin * 2
+
+        imcopy = output_mozaiku.copy() #もとの画像のコピーをとる
+        imcut = imcopy.crop((eye_left_left, eye_top, eye_right_right, eye_bottom)) #目の部分を切り取り
+        mozaiku_size = 4
+        gimg = imcut.resize([mozaiku_size, mozaiku_size]).resize(imcut.size) #目の部分にモザイク処理
+        output_mozaiku.paste(gimg, (eye_left_left, eye_top)) #もとの画像にモザイクをかけた画像をペースト
 
 
-def draw_mozaiku(label, output_mozaiku):
-    imgWidth, imgHeight = output_mozaiku.size
+    def draw_glass(self, output_glass: Image.Image, glass_image: Image.Image):
+        """画像にサングラスを描画する関数
 
-    boxes = label['Landmarks'] # 目なら'Landmarks'を指定
-    for box in boxes:
-        if box['Type'] == 'leftEyeUp':
-            eye_left_top = imgHeight * box['Y']
-        if box['Type'] == 'leftEyeDown':
-            eye_left_bottom = imgHeight * box['Y']
-        if box['Type'] == 'rightEyeUp':
-            eye_right_top = imgHeight * box['Y']
-        if box['Type'] == 'rightEyeDown':
-            eye_right_bottom = imgHeight * box['Y']
-        if box['Type'] == 'leftEyeLeft':
-            eye_left_left = imgWidth * box['X']
-        if box['Type'] == 'leftEyeRight':
-            eye_left_right = imgWidth * box['X']
-        if box['Type'] == 'rightEyeLeft':
-            eye_right_left = imgWidth * box['X']
-        if box['Type'] == 'rightEyeRight':
-            eye_right_right = imgWidth * box['X']
+        Args:
+            output_glass (Image.Image): 出力する画像
+            glass_image (Image.Image): 描画する際に用いるサングラスの画像
 
-    #高さの比較
-    eye_top = min(eye_left_top, eye_right_top)
-    eye_bottom = max(eye_left_bottom, eye_right_bottom)
+        """
 
-    # モザイク範囲を少し広げる
-    margin = (eye_bottom - eye_top) * 1
-    eye_top -= margin
-    eye_bottom += margin * 1.5
-    eye_left_left -= margin * 2
-    eye_right_right += margin * 2
+        imgWidth, imgHeight = output_glass.size
+        glassWidth, glassHeight = glass_image.size
 
-    imcopy = output_mozaiku.copy() #もとの画像のコピーをとる
-    imcut = imcopy.crop((eye_left_left, eye_top, eye_right_right, eye_bottom)) #目の部分を切り取り
-    mozaiku_size = 4
-    gimg = imcut.resize([mozaiku_size, mozaiku_size]).resize(imcut.size) #目の部分にモザイク処理
-    output_mozaiku.paste(gimg, (int(eye_left_left), int(eye_top))) #もとの画像にモザイクをかけた画像をペースト
-    print("mozaiku done")
+        eye_top = int(min(self.eye_left_top, self.eye_right_top) * imgHeight)
+        eye_bottom = int(max(self.eye_left_bottom, self.eye_right_bottom) * imgWidth)
+        eye_left_left = int(self.eye_left_left * imgWidth)
+        eye_right_right = int(self.eye_right_right * imgWidth)
 
-def draw_glass(label, output_glass, glass_image):
-    imgWidth, imgHeight = output_glass.size
-    glassWidth, glassHeight = glass_image.size
+        # サングラスの範囲を少し広げる
+        margin = (eye_bottom - eye_top) * 2
+        eye_top -= margin
+        eye_left_left -= margin * 2
+        eye_right_right += margin * 2
 
-    boxes = label['Landmarks'] # 目なら'Landmarks'を指定
-    for box in boxes:
-        if box['Type'] == 'leftEyeUp':
-            eye_left_top = imgHeight * box['Y']
-        if box['Type'] == 'leftEyeDown':
-            eye_left_bottom = imgHeight * box['Y']
-        if box['Type'] == 'rightEyeUp':
-            eye_right_top = imgHeight * box['Y']
-        if box['Type'] == 'rightEyeDown':
-            eye_right_bottom = imgHeight * box['Y']
-        if box['Type'] == 'leftEyeLeft':
-            eye_left_left = imgWidth * box['X']
-        if box['Type'] == 'rightEyeRight':
-            eye_right_right = imgWidth * box['X']
+        glass_width = (eye_right_right - eye_left_left)
+        glass_height = int(glass_width * (glassHeight / glassWidth))
 
-    #高さの比較
-    eye_top = min(eye_left_top, eye_right_top)
-    eye_bottom = max(eye_left_bottom, eye_right_bottom)
+        glass_image = glass_image.resize((glass_width, glass_height))
+        output_glass.paste(glass_image, (eye_left_left, eye_top), glass_image)
 
-    # 範囲を少し広げる
-    margin = (eye_bottom - eye_top) * 2
-    eye_top -= margin
-    eye_bottom += margin * 1.5
-    eye_left_left -= margin * 2
-    eye_right_right += margin * 2
 
-    glass_width = int(eye_right_right - eye_left_left)
-    glass_height = int(glass_width * (glassHeight / glassWidth))
+class Client:
+    """s3やRekognitionからデータを取得するクラス
 
-    pos = (int(eye_left_left), int(eye_top))
-    glass_image = glass_image.resize((glass_width, glass_height))
-    output_glass.paste(glass_image, pos, glass_image)
+    Attributes:
+        input_bucket (str): input用のバケット名
+        image_path (str): input用のバケットにputされた画像のパス
+        response (dict): s3のデータ
+        rekognition : Rekognitionのクライアント
+        s3_resource : S3のリソース
 
-    print("mozaiku done")
+    """
+
+    def __init__(self, event: dict):
+        """clientで使う値を取得
+
+        Args:
+            event (dict): s3にinputされた際に発生するイベントデータ
+
+        """
+
+        self.input_bucket = event['Records'][0]['s3']['bucket']['name']
+        self.image_path = urllib.parse.unquote_plus(
+                                event['Records'][0]['s3']['object']['key'],
+                                encoding='utf-8'
+                                )
+
+        session = boto3.session.Session()
+        self.response = session.client('s3').list_objects_v2(Bucket=self.input_bucket)
+        self.rekognition = session.client('rekognition')
+        self.s3_resource = session.resource('s3')
+
+    def get_rekognition_labels(self) -> dict:
+        """Amazon Rekognitionからデータを取得する関数
+
+        Returns:
+            dict: 取得したRekognitionデータ
+
+        """
+
+        labels = self.rekognition.detect_faces(
+            Image={
+                "S3Object":
+                    {"Bucket": self.input_bucket,
+                    "Name": self.image_path}},
+            Attributes=['ALL']
+            )
+        return labels
+
+    def get_s3_image(self, img_path: str) -> Image.Image:
+        """s3から画像を取得する関数
+
+        Args:
+            img_path (str): 取得する画像ファイルの絶対パス
+
+        Returns:
+            Image.Image: 読み込んだ画像
+
+        """
+
+        s3_object = self.s3_resource.Object(self.input_bucket, img_path).get()
+        stream = io.BytesIO(s3_object['Body'].read())
+        img = Image.open(stream)
+        return img
